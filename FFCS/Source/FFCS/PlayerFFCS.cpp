3bad6269109fe3181a4252ce348cba634e985c05
Olivier Maurin <154346476+Toraenor@users.noreply.github.com>
2024-12-11 12:18:40 +0100

    continued to transfer bp methods into cpp

--- FFCS/Source/FFCS/PlayerFFCS.cpp
+++ FFCS/Source/FFCS/PlayerFFCS.cpp
@@ -3,3 +3,75 @@
 
 #include "PlayerFFCS.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "InputAction.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APlayerFFCS::APlayerFFCS()
+{
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(GetCapsuleComponent());
+	CameraComp->SetupAttachment(SpringArm);
+	SpringArm->TargetArmLength = 500.f;
+	SpringArm->SetRelativeLocationAndRotation(FVector(0.f, 0.f, 70.f), FRotator(0.f, -17.f, 0.f));
+	SpringArm->bUsePawnControlRotation = true;
+}
+
+void APlayerFFCS::RetrieveIAMoveValuesInCpp(const float ValueX, const float ValueY)
+{
+	InputActionMoveValue.X = ValueX;
+	InputActionMoveValue.Y = ValueY;
+}
+
+float APlayerFFCS::GetMouseDotProduct(const AActor* Enemy) const
+{
+	CameraComp->GetForwardVector().Normalize();
+	return FVector::DotProduct(GetPlayerToEnemyVec(Enemy), CameraComp->GetForwardVector());
+}
+
+float APlayerFFCS::GetKeyboardDotProduct(const AActor* Enemy) const
+{
+	const FRotator Rotation = FRotator(0.f, GetControlRotation().Yaw ,0.f);
+	const FVector RightVec = UKismetMathLibrary::Quat_RotateVector(Rotation.Quaternion(), FVector::RightVector);
+	const FVector ForwardVec = UKismetMathLibrary::Quat_RotateVector(Rotation.Quaternion(), FVector::ForwardVector);
+	FVector Vec = ForwardVec * InputActionMoveValue.Y + RightVec * InputActionMoveValue.X;
+	Vec.Normalize();
+	
+	return FVector::DotProduct(Vec, GetPlayerToEnemyVec(Enemy));
+}
+
+float APlayerFFCS::FindBestInputDotProductWithEnemy(const AActor* Enemy) const
+{
+	if (InputActionMoveValue.Length() != 0.f)
+	{
+		return GetKeyboardDotProduct(Enemy);
+	}
+
+	return GetMouseDotProduct(Enemy);
+}
+
+FVector APlayerFFCS::GetEnemyToPlayerVec(const AActor* Enemy) const
+{
+	FVector EnemyToPlayerVec = GetActorLocation() - Enemy->GetActorLocation();
+	EnemyToPlayerVec.Normalize();
+	return EnemyToPlayerVec;
+}
+
+FVector APlayerFFCS::GetPlayerToEnemyVec(const AActor* Enemy) const
+{
+	FVector PlayerToEnemyVec = Enemy->GetActorLocation() - GetActorLocation();
+	PlayerToEnemyVec.Normalize();
+	return PlayerToEnemyVec;
+}
+
+void APlayerFFCS::FindBestEnemyTest(TArray<const AActor*>& Enemies)
+{
+	// float DotProduct = -1.f;
+	// for (auto Enemy : Enemies)
+	// {
+	// 	
+	// }
+}
--- FFCS/Source/FFCS/PlayerFFCS.h
+++ FFCS/Source/FFCS/PlayerFFCS.h
@@ -10,8 +10,32 @@
  * 
  */
 UCLASS()
+// ReSharper disable once CppUE4CodingStandardNamingViolationWarning
 class FFCS_API APlayerFFCS : public ACharacterBase
 {
 	GENERATED_BODY()
-	
+public:
+	APlayerFFCS();
+
+	UFUNCTION(BlueprintCallable)
+	// ReSharper disable once CppUE4CodingStandardNamingViolationWarning
+	void RetrieveIAMoveValuesInCpp(const float ValueX, const float ValueY);
+	UFUNCTION(BlueprintPure)
+	float GetMouseDotProduct(const AActor* Enemy) const;
+	UFUNCTION(BlueprintPure)
+	float GetKeyboardDotProduct(const AActor* Enemy) const;
+	UFUNCTION(BlueprintPure)
+	float FindBestInputDotProductWithEnemy(const AActor* Enemy) const;
+	UFUNCTION(BlueprintPure)
+	FVector GetEnemyToPlayerVec(const AActor* Enemy) const;
+	UFUNCTION(BlueprintCallable)
+	void FindBestEnemyTest(TArray<const AActor*>& Enemies);
+
+	FVector GetPlayerToEnemyVec(const AActor* Enemy) const;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* CameraComp;
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* SpringArm;
+	FVector2d InputActionMoveValue;
 };
